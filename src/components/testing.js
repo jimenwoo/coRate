9dae24e30c1990c80048cccd64b1d465580c06ef
Tony Liang <liangtonyy@Tonys-MacBook-Pro.local>
2018-12-03 14:52:24 -0800

    testing.js added

---
+++ src/components/testing.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class testing extends Component{
+  render() {
+      return(
+        <div>
+          <h2> hi </h2>
+        </div>
+      )
+  }
+
+
+}
+export default testing;
