0de09e9ab377a96165875ee502ee9240d24a870b
Tony Liang <liangtonyy@Tonys-MacBook-Pro.local>
2018-12-04 23:35:10 -0800

    comments stored in firebase

---
+++ src/components/config.js
@@ -0,0 +1,10 @@
+
+// Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyAb3DecniPkMErcRr2GebYvigm_giFi7YE",
+    authDomain: "corate-4b30a.firebaseapp.com",
+    databaseURL: "https://corate-4b30a.firebaseio.com",
+    projectId: "corate-4b30a",
+    storageBucket: "corate-4b30a.appspot.com",
+    messagingSenderId: "133869785706"
+  };
